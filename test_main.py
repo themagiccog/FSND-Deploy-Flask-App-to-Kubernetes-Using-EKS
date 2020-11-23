2c8f6cfe78c8bee5ef669449482ff99ff243f03e
COG Gbugu <13458634+themagiccog@users.noreply.github.com>
2020-11-23 04:57:08 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ import json
 import pytest
 
 # import main
-import main as __init__
+import main.__init__ as __init__
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
